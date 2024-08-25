74718f7d519ffb033c37c4dfc14a05cdc14c6e30
JJ De Kock <68385686+Rapterian@users.noreply.github.com>
2024-08-25 16:39:36 +0200

    Controllers part 1 finished

--- LibraryManager/src/Controller/AuthorController.java
+++ LibraryManager/src/Controller/AuthorController.java
@@ -1,4 +1,20 @@
 package Controller;
+import Model.AuthorModel;
 
 public class AuthorController {
+    private AuthorModel authorModel;
+
+    public AuthorController(AuthorModel authorModel) {
+        this.authorModel = authorModel;
+        //database add author
+    }
+
+    public AuthorModel getAuthorModel() {
+        return authorModel;
+    }
+
+    public void setAuthorModel(AuthorModel authorModel) {
+        this.authorModel = authorModel;
+    }
+
 }
--- LibraryManager/src/Controller/BookController.java
+++ LibraryManager/src/Controller/BookController.java
@@ -1,4 +1,19 @@
 package Controller;
+import Model.BookModel;
 
 public class BookController {
+    private BookModel bookModel;
+
+    public BookController(BookModel bookModel) {
+        this.bookModel = bookModel;
+        //database add book
+    }
+
+    public BookModel getBookModel() {
+        return bookModel;
+    }
+
+    public void setBookModel(BookModel bookModel) {
+        this.bookModel = bookModel;
+    }
 }
