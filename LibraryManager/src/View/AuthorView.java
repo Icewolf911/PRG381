c4bccf53ebb15047a9997838d4ed81b4b3e8403e
Icewolf911 <110422407+Icewolf911@users.noreply.github.com>
2024-08-25 19:50:05 +0200

    Authors view

--- LibraryManager/src/View/AuthorView.java
+++ LibraryManager/src/View/AuthorView.java
@@ -1,4 +1,17 @@
 package View;
 
+import javax.swing.*;
+
 public class AuthorView {
+    private JPanel panel1;
+    private JTextField txtName;
+    private JTextField txtSurname;
+    private JTextField txtDOB;
+    private JTextField txtEmail;
+    private JTextField txtemail;
+    private JTable tblAuthors;
+    private JButton createButton;
+    private JButton deleteButton;
+    private JButton displayAllButton;
+    private JButton editButton;
 }
